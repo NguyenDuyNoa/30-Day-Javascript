f2407cf2dbde1550c5104356dc3118582d85ab4c
Duy Noa Nguyễn <duynoanguyen@192.168.1.2>
2024-07-03 21:00:03 +0700

    Day 1

---
+++ Day 1/datatypes.js
@@ -0,0 +1,10 @@
+// 5. Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+let x = "Hello word";
+let y = true;
+let z;
+let q = null;
+
+console.log(typeof(x))
+console.log(typeof(y))
+console.log(typeof(z))
+console.log(typeof(q))
---
+++ Day 1/variable.js
@@ -0,0 +1,26 @@
+// 4. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+var a = "Hello word";
+var b = true;
+var d;
+var c = null;
+
+// 6. Declare four variables without assigning values
+let e,f,g,h;
+
+// 7. Declare four variables with assigned values
+let i = 13;
+let j = "Này là biến j";
+let k = null;
+let m;
+
+// 8. Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let firstName = "Noa"
+let lastName = "Nguyễn"
+let marialStatus = "Độc thân"
+let country = "Việt Nam"
+let age = 22
+// 9. Declare variables to store your first name, last name, marital status, country and age in a single line
+let n = 2, l = "20"
+// 10. Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+let myAge = 22, yourAge = 30
+console.log("Tôi ", myAge, " tuổi", "Bạn thì ", yourAge, " tuổi")
